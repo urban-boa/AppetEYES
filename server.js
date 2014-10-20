ef521ad7e3a0d778bf7a4ee5295f2631d09f09de
Chao Xue <xuechao@alum.mit.edu>
2014-10-20 12:50:44 -0700

    Added user auth

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var requestHandler = require('./server/request-handler.js');
+var user = require('./server/user-controller.js');
 var app = express();
 
 app.use(express.static(__dirname + '/app'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+//user-signin routes
+app.post('/users/signin', user.signin);
+app.post('/users/signup', user.signup);
+app.get('/users/signedin', user.checkAuth);
+
+//if the client is not pinging a user-signin route, 
+//then we check if the client is signed in
+app.use(user.decode);
+
+//user-picture history changes
+
+//user-preference change routes
+
+//server listen
 app.listen(process.env.PORT || '8080');
 
 module.exports = app;
---
+++ server/user-controller.js
@@ -0,0 +1,117 @@
+var User = require('./user-model.js'),
+    Q    = require('q'),
+    jwt  = require('jwt-simple');
+
+module.exports = {
+  
+  signin: function (req, res, next) {
+
+    var username = req.body.username,
+        password = req.body.password;
+
+    var findUser = Q.nbind(User.findOne, User);
+    findUser({username: username})
+      .then(function (user) {
+        if (!user) {
+          next(new Error('User does not exist'));
+        } else {
+          return user.comparePasswords(password)
+            .then(function(foundUser) {
+              if (foundUser) {
+                var token = jwt.encode(user, 'secret');
+                res.json({token: token});
+              } else {
+                return next(new Error('No user'));
+              }
+            });
+        }
+      })
+      .fail(function (error) {
+        next(error);
+      });
+
+  },//end signin
+
+  signup: function (req, res, next) {
+
+    var username  = req.body.username,
+        password  = req.body.password,
+        create,
+        newUser;
+
+    var findOne = Q.nbind(User.findOne, User);
+
+    // check to see if user already exists
+    findOne({username: username})
+      .then(function(user) {
+        if (user) {
+          next(new Error('User already exist!'));
+        } else {
+          // make a new user if not one
+          create = Q.nbind(User.create, User);
+          newUser = {
+            username: username,
+            password: password
+          };
+          return create(newUser);
+        }
+      })
+      .then(function (user) {
+        // create token to send back for auth
+        var token = jwt.encode(user, 'secret');
+        res.json({token: token});
+      })
+      .fail(function (error) {
+        next(error);
+      });
+
+  },//end signup
+
+  checkAuth: function (req, res, next) {
+    // checking to see if the user is authenticated
+    // grab the token in the header is any
+    // then decode the token, which we end up being the user object
+    // check to see if that user exists in the database
+    var token = req.headers['x-access-token'];
+    if (!token) {
+      next(new Error('No token'));
+    } else {
+      var user = jwt.decode(token, 'secret');
+      var findUser = Q.nbind(User.findOne, User);
+      findUser({username: user.username})
+        .then(function (foundUser) {
+          if (foundUser) {
+            res.send(200);
+          } else {
+            res.send(401);
+          }
+        })
+        .fail(function (error) {
+          next(error);
+        });
+    }
+  }, //end checkAuth
+
+  decode: function (req, res, next) {
+
+    var token = req.headers['x-access-token'];
+    var user;
+
+    if (!token) {
+      return res.send(403); // send forbidden if a token is not provided
+    }
+
+    try {
+      // decode token and attach user to the request
+      // for use inside our controllers
+      console.log('helpers');
+      user = jwt.decode(token, 'secret');
+      req.username = user.username;
+      next();
+    } catch(error) {
+      return next(error);
+    }
+
+  }//end decode
+
+};
