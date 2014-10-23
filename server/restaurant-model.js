4748f7dd65d8d71fd0e458d5474b1c2904c314f9
Chao Xue <xuechao@alum.mit.edu>
2014-10-23 11:40:41 -0700

    Fixed conflict 2

--- server/restaurant-model.js
+++ server/restaurant-model.js
@@ -8,13 +8,8 @@ var RestaurantSchema = new mongoose.Schema({
   },
   restaurantName: String,
   address: String,
-<<<<<<< HEAD
   phoneNumber: String//,
   //price: Number
-=======
-  phoneNumber: String,
-  price: Number
->>>>>>> 0ad35460dc58588513065392b414b4afa7199f73
 });
 
 module.exports = mongoose.model('restaurant', RestaurantSchema);
