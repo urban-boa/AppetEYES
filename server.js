ed384df6e36fee80d0ee215d663aec33483d169e
Chao Xue <xuechao@alum.mit.edu>
2014-10-23 11:43:52 -0700

    Fixed Conflict 3

--- server.js
+++ server.js
@@ -1,28 +1,22 @@
 var express = require('express');
-
 var bodyParser = require('body-parser');
-<<<<<<< HEAD
 var mongoose = require('mongoose');
 var user = require('./server/user-controller.js');
 var imageInfo = require('./server/image-restaurant-controller.js');
 var image = require('./server/image-controller.js');
-var app = express();
-var mongodb = process.env.IP;
-
-mongoose.connect(mongodb || 'mongodb://localhost/appetEYES');
-=======
 var user = require('./server/user-controller.js');
 var yelp = require('./server/api/yelp.controller.js');
 var app = express();
-var mongoose    = require('mongoose');
->>>>>>> 0ad35460dc58588513065392b414b4afa7199f73
+
 app.use(express.static(__dirname + '/app'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 console.log('Starting APP');
+
 //Mongo DB Connection
 var mongoUrl = process.env.port ? "mongodb://MongoLab-s:mFZxpWu8Pi.eNBqD4fBHxSXXg0DI.egdrUzjN8j3lsA-@ds041157.mongolab.com:41157/MongoLab-s" : 'mongodb://localhost/appteys';
 mongoose.connect(mongoUrl);
+
 //user-signin routes
 app.post('/users/signin', user.signin);
 app.post('/users/signup', user.signup);
@@ -48,7 +42,6 @@ app.get('/restaurant/info', imageInfo.getRestaurantInfo);
 app.post('/restaurant/info', image.saveRestaurant);
 
 //server listen
-
 app.listen(process.env.PORT || '8080');
 
 module.exports = app;
