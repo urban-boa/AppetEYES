6cd9c43267e477e3924ae3f85df1aa61d1160164
Chao Xue <xue0chao@users.noreply.github.com>
2014-10-23 16:48:49 -0700

    Delete userImages.js

--- server/userImages.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var UserImageSchema = mongoose.Schema({
-  userID: String,
-  imageID: String,
-  liked: Boolean
-});
-
-module.exports = mongoose.model('userImage', UserImageSchema);
