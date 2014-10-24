2cf0c1a6589cf51a41410ff32fdc4d9f25f4a06c
bace135 <bace135@users.noreply.github.com>
2014-10-24 12:46:08 -0700

    Update app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -96,7 +96,43 @@ angular.module('Appeteyes', ['ionic', 'config', 'Appeteyes.controllers', 'Appete
           controller: 'AccountCtrl'
         }
       }
+    })
+    
+    .state('tab.preferences', {
+      url: '/preferences',
+      views: {
+        'tab-preferences': {
+          templateUrl: 'templates/tab-preferences.html',
+          controller: 'PreferencesCtrl'
+        }
+      }
     });
+
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/dash');
+
+  //We add $httpInterceptor into the array
+  //$httpProvider.httpInterceptor.push('AttachTokens');
+})
+
+// .factory('AttachTokens', function ($window) {
+//   // this is an $httpInterceptor
+//   // its job is to stop all out going request
+//   // then look in local storage and find the user's token
+//   // then add it to the header so the server can validate the request
+//   var attach = {
+//     request: function (object) {
+//       var jwt = $window.localStorage.getItem('com.shortly');
+//       if (jwt) {
+//         object.headers['x-access-token'] = jwt;
+//       }
+//       object.headers['Allow-Control-Allow-Origin'] = '*';
+//       return object;
+//     }
+//   };
+//   return attach;
+// });
 
 
   // if none of the above states are matched, use this as the fallback
