974847b97f2e7f4cd629b60e77f8ad79d3748dae
bace135 <bryane@rocketship.com>
2014-10-27 12:18:20 -0700

    details working again

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -82,7 +82,7 @@ angular.module('Appeteyes', ['ionic', 'config', 'Appeteyes.controllers', 'Appete
     .state('tab.foodDetail', {
       url: '/myFoodies/{name}',
       views: {
-        'foodDetail': {
+        'tab-myFoodies': {
           templateUrl: 'templates/foodDetail.html',
           controller: 'FoodDetailCtrl'
         }
