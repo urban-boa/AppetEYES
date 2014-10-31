39fabc58604ac1dd2961f6d675bc9444686464aa
Teresa Yung <tereyung@gmail.com>
2014-10-31 09:38:30 -0700

    logo now flips

--- app/scripts/account-controller.js
+++ app/scripts/account-controller.js
@@ -1,12 +1,13 @@
 'use strict';
+// angular.module('Appeteyes.controllers', ['famous.angular'])
 angular.module('Appeteyes.controllers')
 
-.controller('AccountCtrl', function($scope, Auth, Fooder) {
+.controller('AccountCtrl', ['$scope', '$famous', function($scope, $famous, Auth, Fooder) {
   //$scope.user.username and $scope.user.password are being used as ng-models on the template URL tab-account
   $scope.user = {
     content: "Change me!"
   };
-  // console.log('Form');
+  console.log('am i here?');
 
   $scope.submitForm = function(){
     Auth.login($scope.user);
@@ -19,5 +20,19 @@ angular.module('Appeteyes.controllers')
   $scope.signOut = function(){
     Auth.signout();
   };
+  $scope.flipIt = function() {
+    console.log( "am i in flip?")
+    $famous.find('fa-flipper')[0].flip();
+  };
+  $scope.flipItt = function() {
+    console.log( "am i in flip2?")
+    $famous.find('fa-flipper')[0].flip();
+  };
 
-});
+}]);
+
+// .controller('FlipperCtrl', ['$scope', '$famous', function($scope, $famous) {
+    
+// }]);
+
+
